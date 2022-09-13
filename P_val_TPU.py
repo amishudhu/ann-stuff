359b7be99a7f850491ef4b5177c9691f55fbf30f
amishudhu <113205665+amishudhu@users.noreply.github.com>
2022-09-13 17:06:48 -0300

    Update P_val_TPU.py

--- P_val_TPU.py
+++ P_val_TPU.py
@@ -19,6 +19,7 @@ pd.options.display.float_format = '{:40,.8f}'.format
 np.seterr(divide='ignore')
 from sklearn.model_selection import train_test_split
 from keras.callbacks import EarlyStopping
+
 cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
 tf.config.experimental_connect_to_cluster(cluster_resolver)
 tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
