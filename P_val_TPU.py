0b30299ea0b86fb0646fa3ed2e23566b4c3c61f0
amishudhu <113205665+amishudhu@users.noreply.github.com>
2022-09-14 14:15:29 -0300

    Update P_val_TPU.py

--- P_val_TPU.py
+++ P_val_TPU.py
@@ -91,10 +91,12 @@ def p_val_est():
         Gradient_sq_sum.loc[z]=[np.mean(np.square(Gradient_list[:,0])),np.mean(np.square(Gradient_list[:,1])),np.mean(np.square(Gradient_list[:,2])),np.mean(np.square(Gradient_list[:,3])),np.mean(np.square(Gradient_list[:,4]))]
     for m in range(len(Gradient_sq_sum.columns)):
         print(f'The 90th, 95th and 99th percentiles for {list(Traintest[0].columns.values)[m]} are: ' + repr(np.percentile(Gradient_sq_sum[Gradient_sq_sum.columns[m]], 90)) + ',' + repr(np.percentile(Gradient_sq_sum[Gradient_sq_sum.columns[m]], 95)) + ', and ' + repr(np.percentile(Gradient_sq_sum[Gradient_sq_sum.columns[m]], 99))+ '.')
-cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
-tf.config.experimental_connect_to_cluster(cluster_resolver)
-tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
-strategy = tf.distribute.TPUStrategy(cluster_resolver)
+import tensorflow as tf
+
+resolver = tf.distribute.cluster_resolver.TPUClusterResolver()
+tf.config.experimental_connect_to_cluster(resolver)
+tf.tpu.experimental.initialize_tpu_system(resolver)
+strategy = tf.distribute.experimental.TPUStrategy(resolver)
 full=strategy.run(p_val_est())
        
     
